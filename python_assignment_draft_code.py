fd0ebfc4a1fd0e437259bd08491628074b175437
michelleczajkowski <127299519+michelleczajkowski@users.noreply.github.com>
2023-05-11 09:33:19 +0200

    use python_assignment_draft_code from now

--- python_assignment_draft_code.py
+++ python_assignment_draft_code.py
@@ -1,36 +1,82 @@
-import pandas as pd
-import random
 import tkinter as tk
-from PIL import ImageTk, Image
+from PIL import Image, ImageTk
+import time
+import random
+import keyboard
+import pandas as pd
 import time
 import keyboard 
+import os
 
-########## GET THE WORD AND PICS FROM THE STIMULUS SET
+# Set up the stimulus
+stimulus = pd.read_csv("stimulus.csv")
 
-# Read the CSV file into a dataframe
-stimulus = pd.read_csv('stimulus.csv')
+# Select a random row from the data set
+row = stimulus.sample()
 
 # Select a random word from the 'word' column
-word = random.choice(stimulus['word'].tolist())
-image_name = stimulus.loc[stimulus['word'] == word, 'correct'].iloc[0]
-image_path = f"images/{image_name}"  # assuming images are in the 'images' subdirectory
-print(word)
-print(image_name)
-print(image_path)
+word = row.iloc[0, 0]
 
-##########
+# select the value from the other rows randomly 
+value_col = random.choice([1, 2, 3])
+image_path = f"images/{row.iloc[0, value_col]}"
 
-print("Press 'g' key to start timer")
+# Create a blank window
+window = tk.Tk()
+window.title("Stimulus")
+window.geometry("800x600")
+window.attributes("-topmost", True)
+canvas = tk.Canvas(window, width=800, height=600, bg='black')
+canvas.pack()
+canvas.create_text(400, 300, text="ready? go!", fill='white', font=('Arial', 100), anchor=tk.CENTER)
+canvas.update()
+keyboard.wait('c')
+#window.after(1000)
+canvas.delete("all")
+canvas.update()
+canvas.create_text(400, 300, text=word, fill='white', font=('Arial', 100), anchor=tk.CENTER)
+canvas.update()
+window.after(1000)
+canvas.delete("all")
+canvas.update()
+window.after(1000)
+# Load the image using PIL
+image = Image.open(image_path)
 
-# wait for 'g' key press
-keyboard.wait('g')
-print("g pressed. press 's' to stop")
-start_time = time.time() * 1000  # convert to milliseconds
+# Convert the image to a PhotoImage object
+tk_image = ImageTk.PhotoImage(image)
 
-# wait for 's' key press to stop the timer
-keyboard.wait('s')
+# Create an image item on the canvas using the PhotoImage object
+canvas.create_image(400, 300, image=tk_image)
 
-end_time = time.time() * 1000  # convert to milliseconds
+# Pack the canvas so it appears in the window
+canvas.pack()
+
+# Function to close the window when a key is pressed
+def key_press(event):
+    global key
+    key = event.char
+    window.destroy()
+
+# Bind the key press event to the function
+window.bind("<Key>", key_press)
+
+# Show the window
+window.mainloop()
+
+# Calculate the reaction time
+start_time = time.time() * 1000
+end_time = start_time
+
+# Wait for a key press event
+while end_time == start_time:
+    end_time = time.time() * 1000
+
+# Calculate the duration
 duration = end_time - start_time
 
-print(f"Elapsed time: {duration:.2f} milliseconds")
+# Print the results
+print(word)
+print(image_path)
+print(key)
+print(duration)
